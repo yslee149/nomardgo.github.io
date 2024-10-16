18f0bf91bc65367a878ca30ac0622edb749748f4
이윤성 <yslee149@gmail.com>
2024-10-16 16:12:01 +0900

    123

--- com.hello/com/hello/Hello.java
+++ com.hello/com/hello/Hello.java
@@ -1,7 +1,41 @@
-
+import java.util.List;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        
+
+        list = new ArrayList<>();
+        Node node = new Node("1");
+        list.add(node);
+
+        Node node2 = new Node("2");
+        node.next = node2;
+        
+        Node node3 = new Node("3");
+        node2.next = node3;
+
+    }
+
+    // 리스트 구조인데 하위 목록이 많은 데이터 
+    
+    List<Node> list;
+
+    class Node {
+
+        Node(String data) {
+            this.data = data;
+        }
+
+        String data;
+        Node next;
+
+        List<Node> childList;
+
+        public void addChild(Node node) {
+            if (childList == null) {
+                childList = new ArrayList<>();
+            }
+            childList.add(node);
+        }
     }
 }
