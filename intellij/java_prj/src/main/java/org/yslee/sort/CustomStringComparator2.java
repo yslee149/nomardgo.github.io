78d1907e5cdba7d70defb55fea0a9dc99836cb5a
이윤성 <yslee149@gmail.com>
2024-10-18 18:10:55 +0900

    '123'

--- intellij/java_prj/src/main/java/org/yslee/sort/CustomStringComparator2.java
+++ intellij/java_prj/src/main/java/org/yslee/sort/CustomStringComparator2.java
@@ -37,3 +37,67 @@ public class CustomStringComparator2<T> implements Comparator<T> {
         return processedS1.compareTo(processedS2);
     }
 }
+
+////////////
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class AdvancedNaturalSortExample {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("aaa.0");
+        list.add("aaa.1");
+        list.add("aaa.2");
+        list.add("aaa.11");
+        list.add("bbb.2.1");
+        list.add("bbb.2.10");
+        list.add("bbb.2.2");
+
+        // 커스텀 Comparator 사용
+        Collections.sort(list, new AdvancedNaturalOrderComparator());
+
+        // 정렬 결과 출력
+        for (String str : list) {
+            System.out.println(str);
+        }
+    }
+}
+
+// 여러 숫자 부분을 처리하는 자연스러운 순서 Comparator
+class AdvancedNaturalOrderComparator implements Comparator<String> {
+    @Override
+    public int compare(String s1, String s2) {
+        String[] tokens1 = s1.split("\\.");
+        String[] tokens2 = s2.split("\\.");
+
+        int len = Math.min(tokens1.length, tokens2.length);
+        for (int i = 0; i < len; i++) {
+            String token1 = tokens1[i];
+            String token2 = tokens2[i];
+
+            boolean isNumeric1 = token1.matches("\\d+");
+            boolean isNumeric2 = token2.matches("\\d+");
+
+            if (isNumeric1 && isNumeric2) {
+                // 숫자 부분을 정수로 비교
+                int num1 = Integer.parseInt(token1);
+                int num2 = Integer.parseInt(token2);
+                if (num1 != num2) {
+                    return Integer.compare(num1, num2);
+                }
+            } else {
+                // 문자열 부분을 비교
+                int cmp = token1.compareTo(token2);
+                if (cmp != 0) {
+                    return cmp;
+                }
+            }
+        }
+
+        // 토큰 수에 따라 비교
+        return Integer.compare(tokens1.length, tokens2.length);
+    }
+}
